9653895bc5057182d41cde52a9922b21b6f3a9af
ToniAlvarez <aam0021@alu.medac.es>
2025-03-11 23:52:55 +0100

    Fix: Deploy-ready build for Netlify

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { FaUserCircle } from "react-icons/fa";
+import { MdAccountCircle } from "react-icons/md";
 import { useState } from "react";
 
 const Navbar = () => {
@@ -14,29 +14,32 @@ const Navbar = () => {
         <Link to="/videojuegos" className="text-white">Videojuegos</Link>
 
         <div className="relative">
-          <button onClick={() => setMenuOpen(!menuOpen)} className="text-white text-3xl">
-            <FaUserCircle />
-          </button>
+      <button 
+        onClick={() => setMenuOpen(!menuOpen)} 
+        className="text-white text-3xl focus:outline-none"
+      >
+        <MdAccountCircle />
+      </button>
 
-          {menuOpen && (
-            <div className="absolute right-0 mt-2 w-48 bg-gray-700 rounded-lg shadow-lg py-2">
-              <Link 
-                to="/favoritos" 
-                className="block px-4 py-2 text-white hover:bg-gray-600"
-                onClick={() => setMenuOpen(false)}
-              >
-                Mis Favoritos
-              </Link>
-              <Link 
-                to="/eventos" 
-                className="block px-4 py-2 text-white hover:bg-gray-600"
-                onClick={() => setMenuOpen(false)}
-              >
-                Mis Eventos
-              </Link>
-            </div>
-          )}
+      {menuOpen && (
+        <div className="absolute right-0 mt-2 w-48 bg-gray-700 rounded-lg shadow-lg py-2 z-50">
+          <Link 
+            to="/favoritos" 
+            className="block px-4 py-2 text-white hover:bg-gray-600"
+            onClick={() => setMenuOpen(false)}
+          >
+            Mis Favoritos
+          </Link>
+          <Link 
+            to="/eventos" 
+            className="block px-4 py-2 text-white hover:bg-gray-600"
+            onClick={() => setMenuOpen(false)}
+          >
+            Mis Eventos
+          </Link>
         </div>
+      )}
+    </div>
       </div>
     </nav>
   );
